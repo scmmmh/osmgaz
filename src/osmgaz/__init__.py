e3f8c99a408d0146dabc6355a55cba65c8974b4c
Mark <mark.hall@work.room3b.eu>
2015-10-29 17:37:31 +0000

    Added a top-level OSMGaz object.

--- src/osmgaz/__init__.py
+++ src/osmgaz/__init__.py
@@ -1,13 +1,53 @@
 from sqlalchemy import and_
+from geoalchemy2.shape import to_shape
+from shapely import wkb
 
 from .gazetteer import ContainmentGazetteer, ProximalGazetteer
 from .filters import ContainmentFilter, ProximalFilter
 from .classifier import NameSalienceCalculator, TypeSalienceCalculator
 from .models import Point, Line, Polygon
 
+class OSMGaz(object):
+    """Main interface object, handles the full gazetteer pipeline.
+    """
+
+    def __init__(self, sqlalchemy_uri):
+        self.containment_gaz = ContainmentGazetteer(sqlalchemy_uri)
+        self.containment_filter = ContainmentFilter(self.containment_gaz)
+        self.proximal_gaz = ProximalGazetteer(sqlalchemy_uri)
+        self.proximal_filter = ProximalFilter(self.proximal_gaz)
+        self.name_salience_calculator = NameSalienceCalculator(sqlalchemy_uri)
+        self.type_salience_calculator = TypeSalienceCalculator(sqlalchemy_uri)
+
+    def __call__(self, point):
+        """Run the gazetteer pipeline for a single point. Returns a dictionary with
+        containment and proximal toponyms. The containment toponyms are sorted by
+        containment hierarchy. The proximal toponyms are in a random order.
+        """
+        def format_topo(toponym, classification, name_salience=None, type_salience=None):
+            data = {'name': toponym.name,
+                    'geometry': wkb.dumps(to_shape(toponym.way)),
+                    'type': classification['type']}
+            if name_salience is not None or type_salience is not None:
+                data['salience'] = {}
+                if name_salience is not None:
+                    data['salience']['name'] = name_salience
+                if type_salience is not None:
+                    data['salience']['type'] = type_salience
+            return data
+        containment = self.containment_gaz(point)
+        filtered_containment = self.containment_filter(containment)
+        proximal = self.proximal_gaz(point, filtered_containment)
+        filtered_proximal = self.proximal_filter(proximal, point, containment)
+        
+        return {'containment': [format_topo(t, c) for (t, c) in filtered_containment],
+                'proximal': [format_topo(t, c, self.name_salience_calculator(t, filtered_containment), self.type_salience_calculator(c, filtered_containment))
+                             for (t, c) in filtered_proximal]}
+
+
 def main():
-    with open('unknown.txt', 'w') as _:
-        pass
+    #with open('unknown.txt', 'w') as _:
+    #    pass
     points = [
               (-2.63629, 53.39797), # Dakota Park
               (-1.88313, 53.38129), # Peak District
@@ -17,17 +57,13 @@ def main():
               (-2.04045, 53.34058), # Lyme Park
               (-2.47429, 53.3827),  # Lymm
               ]
-    containment_gaz = ContainmentGazetteer('postgresql+psycopg2://osm:osmPWD@localhost:4321/osm')
-    containment_filter = ContainmentFilter(containment_gaz)
-    proximal_gaz = ProximalGazetteer('postgresql+psycopg2://osm:osmPWD@localhost:4321/osm')
-    proximal_filter = ProximalFilter(proximal_gaz)
-    name_salience_calculator = NameSalienceCalculator('postgresql+psycopg2://osm:osmPWD@localhost:4321/osm')
-    type_salience_calculator = TypeSalienceCalculator('postgresql+psycopg2://osm:osmPWD@localhost:4321/osm')
+    gaz = OSMGaz('postgresql+psycopg2://osm:osmPWD@localhost:4321/osm')
     for point in points:
         print(point)
-        containment = containment_gaz(point)
-        filtered_containment = containment_filter(containment)
-        print(', '.join([t.name for t, _ in filtered_containment]))
+        data = gaz(point)
+        print(', '.join([t['name'] for t in data['containment']]))
+        print('\n'.join(['%s - %s (%.4f %.4f)' % (t['name'], t['type'], t['salience']['name'], t['salience']['type']) for t in data['proximal']]))
+
         # Find any unclassified toponyms
         """for pnt in name_salience_calculator.session.query(Point).filter(and_(Point.name != '',
                                                                                Point.classification == None,
@@ -49,18 +85,10 @@ def main():
                 polygon.classification = '::'.join(classification['type'])
         name_salience_calculator.session.commit()"""
         #####
-        proximal = proximal_gaz(point, filtered_containment)
-        filtered_proximal = proximal_filter(proximal, point, containment)
-        for toponym, type_ in filtered_proximal:
-            print(toponym.name, type_['type'], name_salience_calculator(toponym, filtered_containment), type_salience_calculator(type_, filtered_containment))
-
-        with open('unknown.txt', 'a') as out_f:
-            for unknown in type_salience_calculator.classifier.get_unknown():
-                out_f.write('%s\n' % repr(unknown))
     with open('unknown.txt', 'a') as out_f:
-        for tags in containment_gaz.classifier.get_unknown():
+        for tags in gaz.containment_gaz.classifier.get_unknown():
             out_f.write('%s\n' % repr(tags))
-        for tags in proximal_gaz.classifier.get_unknown():
+        for tags in gaz.proximal_gaz.classifier.get_unknown():
             out_f.write('%s\n' % repr(tags))
 
 """
