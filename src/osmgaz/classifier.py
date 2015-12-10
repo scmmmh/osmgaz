c6211b67b854abc069aee4d90c90f68c899f7d49
Mark <mark.hall@work.room3b.eu>
2015-12-10 16:15:36 +0000

    Ensure the ignore rules are run first when classifying.

--- src/osmgaz/classifier.py
+++ src/osmgaz/classifier.py
@@ -63,32 +63,34 @@ class ToponymClassifier(object):
         self.rules.sort(key=lambda r: (len(r['rules']), len(r['type'])), reverse=True)
         
         # Add static ignore rules
-        self.rules.extend([{'rules': {'public_transport': 'pay_scale_area'}},
-                           {'rules': {'boundary': 'vice_county'}},
-                           {'rules': {'boundary': 'political'}},
-                           {'rules': {'boundary': 'administrative', 'admin_level': '5'}},
-                           {'rules': {'boundary': 'police'}},
-                           {'rules': {'boundary': 'statistical'}},
-                           {'rules': {'route': 'train'}},
-                           {'rules': {'route': 'foot'}},
-                           {'rules': {'route': 'bus'}},
-                           {'rules': {'route': 'railway'}},
-                           {'rules': {'route': 'bicycle'}},
-                           {'rules': {'route': 'hiking'}},
-                           {'rules': {'route': 'mtb'}},
-                           {'rules': {'route': 'ferry'}},
-                           {'rules': {'route': 'tram'}},
-                           {'rules': {'route': 'road'}},
-                           {'rules': {'power': 'line'}},
-                           {'rules': {'wpt_symbol': 'Waypoint'}},
-                           {'rules': {'wpt_symbol': 'Crossing'}},
-                           {'rules': {'amenity': 'dog_agility_obstacle'}},
-                           {'rules': {'building': 'entrance'}},
-                           {'rules': {'entrance': 'residence'}},
-                           {'rules': {'amenity': 'vending_machine'}},
-                           {'rules': {'amenity': 'charging_station'}},
-                           {'rules': {'amenity': 'postbox'}},
-                           {'rules': {'pipeline': 'inspection_chamber'}}])
+        for rule in [{'public_transport': 'pay_scale_area'},
+                     {'boundary': 'vice_county'},
+                     {'boundary': 'political'},
+                     {'boundary': 'administrative', 'admin_level': '5'},
+                     {'boundary': 'police'},
+                     {'boundary': 'statistical'},
+                     {'route': 'train'},
+                     {'route': 'foot'},
+                     {'route': 'bus'},
+                     {'route': 'railway'},
+                     {'route': 'bicycle'},
+                     {'route': 'hiking'},
+                     {'route': 'mtb'},
+                     {'route': 'ferry'},
+                     {'route': 'tram'},
+                     {'route': 'road'},
+                     {'power': 'line'},
+                     {'wpt_symbol': 'Waypoint'},
+                     {'wpt_symbol': 'Crossing'},
+                     {'amenity': 'dog_agility_obstacle'},
+                     {'building': 'entrance'},
+                     {'entrance': 'residence'},
+                     {'amenity': 'vending_machine'},
+                     {'amenity': 'charging_station'},
+                     {'amenity': 'postbox'},
+                     {'pipeline': 'inspection_chamber'},
+                     {'place': 'subdivision'}]:
+            self.rules.insert(0, {'rules': rule})
     
     def get_unknown(self):
         """Return the list of all unknown toponym types.
@@ -104,7 +106,7 @@ class ToponymClassifier(object):
         for kw in ['name', 'phone', 'wikipedia', 'route_name', 'route_pref_color', 'way_area', 'building:part',
                    'website', 'wheelchair', 'postal_code', 'license_notice', 'description', 'operator', 'alt_name',
                    'email', 'old_name', 'date', 'opening_hours', 'genus', 'inscription', 'url', 'height', 'ref',
-                   'direction', 'is_in', 'species', 'wpt_description', 'wpt_symbol', 'ele']:
+                   'direction', 'is_in', 'species', 'wpt_description', 'wpt_symbol', 'ele', 'capacity', 'occupier']:
             if kw in tags:
                 del tags[kw]
         for tag in list(tags):
